383f981a7bd3de6e24a7ccece7b06c843189ef71
unknown <amartyabiswas001@gmail.com>
2018-03-20 23:46:31 +0530

    Payment gateway using Stripe

---
+++ server.js
@@ -0,0 +1,42 @@
+let express=require('express');
+let stripe=require('stripe')('sk_test_kR4BF5lebtJxh7mzwDEtxYf0');
+let hbs= require('hbs');
+let bodyParser= require('body-parser');
+
+let app =express();
+app.set('view engine', 'hbs');
+app.set('views', __dirname+'/views');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+app.get('/paysuccess', function (req, res) {
+    res.render('paysuccess');
+});
+
+app.post('/charge', function (req, res) {
+
+    let token = req.body.stripeToken; // Using Express
+    
+    let charge=stripe.charges.create({
+        amount: 2999,
+        currency: "usd",
+        description: "Example charge",
+        source: token
+    }, function(err, charge) {
+        // asynchronously called
+        if(err){
+            console.log(err);
+        }else{
+            console.log(charge);
+        }
+    });
+    res.redirect('/paysuccess')
+});
+
+app.listen(3000, function () {
+   console.log('Magic happens at port 3000');
+});
